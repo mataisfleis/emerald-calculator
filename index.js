8149ef46c0c2047cca92443c1e3a33194162b85d
Mataisplays <mateo20102020@gmail.com>
2023-11-10 19:56:55 -0600

    Add errors
    Added errors with an NaN and numbers equals or lower than 0

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ boton.addEventListener("click", function () {
     var resultEl = document.getElementById("result");
     var inputCalc = inputElement.value;
     var result = (inputCalc * 4)
+    if(isNaN(result)){
+        result = "Error: Input is Not a number"
+    }else if(result <= 0) result = "Error: Input must be greater than 0"
     resultEl.innerHTML = result
-    console.log("Texto ingresado: " + inputCalc + " Resultado" + result);
+    console.log("Texto ingresado: " + inputCalc + " Resultado: " + result);
+    console.log(result);
 });
